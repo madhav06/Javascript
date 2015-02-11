72a61956aff5dfeeba75c2c8d1b8d43af40da1fa
Madhav Nandan <mnandan06@gmail.com>
2015-02-12 00:00:58 +0530

    script15 - script20

---
+++ script15.js
@@ -0,0 +1,9 @@
+if ( "madhav".length >= 7) 
+{
+    console.log("Let's go down the first road!");
+}
+else 
+{
+    // What should we do if the condition is false? Fill in here:
+    console.log("You have a short name!");
+}
---
+++ script16.js
@@ -0,0 +1,15 @@
+// Remember, the order and punctuation matter.
+// If you get an error, check carefully, line by line.
+// If you're really stuck, click "Stuck? Get a hint!"
+
+if ("madhav nandan".length <= 5) 
+{
+    // if condition is true
+    // do this code
+    console.log("This condition is True");
+}
+else // "otherwise"
+{
+    // do this code instead
+    console.log("This condition is False!");
+}
